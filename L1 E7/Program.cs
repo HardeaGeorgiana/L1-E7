60fce23ef04bb7cb511fa20ead11de8c81cc45ae
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-11-28 11:13:26 +0200

    Add project files.

---
+++ L1 E7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace L1_E7
+{
+    /* Scrieti un program care va verifica daca un numar
+       citit de la tastatura este par sau impar
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numar;
+
+            Console.WriteLine("Introduceti un numar");
+            numar = int.Parse(Console.ReadLine());
+
+            if (numar % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+            
+        }
+    }
+}
